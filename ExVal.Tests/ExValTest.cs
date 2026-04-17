3e84988564e97827d63d9fcf66764b4d34e9be9d
Rekroma <raugustlena@gmail.com>
2026-04-17 15:56:18 +0200

    Updated Tests for more inclusive testing.

--- ExVal.Tests/ExValTest.cs
+++ ExVal.Tests/ExValTest.cs
@@ -1,4 +1,6 @@
-﻿using ExVal.App.Validator;
+﻿using ExVal.App.Models;
+using ExVal.App.Services;
+using ExVal.App.Validator;
 
 namespace ExVal.Tests;
 
@@ -6,10 +8,18 @@ namespace ExVal.Tests;
 public class Tests
 {
     [Test]
-    public void InputValidatorTest()
+    public void TestAll()
     {
-        bool expected = false;
-        bool actual = InputValidator.IsValid("");
-        Assert.That(expected, Is.EqualTo(actual)); 
+        var testCases = new Dictionary<string, double>
+        {
+            ["1 + 2 - 3"] = 0,
+            ["1 * ( 2 + 3 ) - 4"] = 1,
+            ["( 1 + 2 ) * 3 / 4"] = 2.25,
+        };
+
+        foreach (var testCase in testCases)
+        {
+            Assert.That(Evaluator.ResolveAST(new Lexer().Parse(testCase.Key)), Is.EqualTo(testCase.Value));
+        }
     }
 }
